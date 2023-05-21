97f29506c2bb35c067bff6a4bd844dc6d9889c23
Fuachua <123046331+Fuachua@users.noreply.github.com>
2023-05-21 15:51:08 -0600

    Update jueo.js

--- jueo.js
+++ jueo.js
@@ -1,12 +1,5 @@
-var canvas = document.getElementById('game-canvas');
-var ctx = canvas.getContext('2d');
-var isInvincible = false;
-
-// Lógica del juego y dibujo del dinosaurio
-
 document.addEventListener('keydown', function(event) {
-  if (event.keyCode === 86) { // Código de tecla para la letra "v"
-    isInvincible = true;
-    // Lógica para activar la invencibilidad
+  if (event.key === 'v' || event.key === 'V') {
+    alert('Game Over');
   }
 });
